315f973c627400b20abf3495ac61e89926f6c933
Alena Kozyreva <alena.kozyreva@paygine.net>
2023-11-23 01:11:58 +0300

    done dz2

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,32 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Привет, мир!");
-        }
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+        var inc = 4;
+        var decDog = dog-3.5;
+        var decCat = cat-1.6;
+        var decPaper = paper-7639;
+        var friend = 19;
+        var frog =3.5;
+        var onePerson = 78.2;
+        var twoPerson = 82.7;
+        var hoursPeople = 640;
+        var hoursPerson = 8;
+        var allWorkPeople = (hoursPeople/hoursPerson)+94 ;
+
+
+        System.out.println(cat + "   " + dog + "   " + paper);
+        System.out.println((cat + inc) + "   " + (dog + inc) + "  " + (paper + inc) );
+        System.out.println(decCat + "   " + decDog + "   " + decPaper);
+        System.out.println(friend + "    " + (friend + 2) + "    " + (friend /7));
+        System.out.println(frog + "   " + (frog * 10) + "  " + (frog * 10 /3.5) +  "   " + ((frog * 10 /3.5)+4) );
+        System.out.println((onePerson + twoPerson) + "    " + (twoPerson - onePerson));
+        System.out.println((onePerson + twoPerson) + "    " + (twoPerson %onePerson));
+        System.out.println("«Всего работников в компании - " + (hoursPeople / hoursPerson) + " человек»."  );
+        System.out.println("«Если в компании работает " + allWorkPeople + " человек, "  + " то всего  "  + (hoursPeople / allWorkPeople) +  " часов работы может быть поделено между сотрудниками».");
+
+
+    }
 }
