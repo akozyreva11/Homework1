e097a75a8ecdb270c547af9c3d7733f3e5c2597b
Alena Kozyreva <alena.kozyreva@paygine.net>
2024-02-08 00:41:50 +0300

    kursovai correct level 1

--- src/Employee.java
+++ src/Employee.java
@@ -1,39 +1,22 @@
+import java.util.Objects;
+
 public class Employee {
 
+    public static int id = 0;
     private String fioName;
-
-    public int getId() {
-        return id;
-    }
-
-    int id=Main.id;
-
-
     private int sallary;
-
     private int department;
 
 
-    @Override
-    public String toString() {
-        return fioName + "  " +  "  " +
-                sallary + "  " +  "  " +
-                department;
-    }
-
     public Employee(String fioName, int department, int sallary) {
-        this.id =getId();
         this.fioName = fioName;
         this.department = department;
         this.sallary = sallary;
     }
 
+
     public int getSallary() {
         return sallary;
-    }
-
-    public int getDepartment() {
-        return department;
     }
 
     public String getFioName() {
@@ -47,5 +30,23 @@ public class Employee {
     public void setSallary(int sallary) {
         this.sallary = sallary;
     }
+    @Override
+    public String toString() {
+        return fioName + "  " +  "  " +
+                sallary + "  " +  "  " +
+                department;
+    }
+    // не знаю надо было или нет , но добавила equals/hashCode увидела у тебя в комменте ,что ты их перечислили вместе с toString
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return sallary == employee.sallary && department == employee.department && Objects.equals(fioName, employee.fioName);
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(fioName, sallary, department);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
-    public static int id = 0;
+
     public static Employee[] employee = new Employee[10];
 
     public static void main(String[] args) {
-        employee[0] = (new Employee("Иванов Петр Петрович", 1, 0));
+        employee[0] = (new Employee("Иванов Петр Петрович", 1, 2));
         employee[1] = (new Employee("Cидоров Вольф Ветрович", 2, 800));
         employee[2] = (new Employee("Иванова Тестовая Женщина", 3, 100));
         employee[3] = (new Employee("Карлондий Карл Витальевич", 4, 300));
@@ -13,7 +13,7 @@ public class Main {
         employee[7] = (new Employee("Козырева Козырела Козыревна", 3, 8900));
         employee[8] = (new Employee("Осипова Ирина Семёновна", 4, 7800));
         employee[9] = (new Employee("Кохинкина Петруня Сергеевна", 5, 56000));
-        employee[0].setDepartment(1);
+        employee[0].setDepartment(4);
         employee[0].setSallary(9000);
 
         printAll();
@@ -28,12 +28,13 @@ public class Main {
 
     }
 
+
     // список всех сотрудников со всеми данными,
     public static void printAll() {
-        for (; id < employee.length-2; id++) {
-            System.out.println(id + " " + employee[id]);
+        for (int i = 0; i < employee.length; i++) {
+            Employee.id++;
+            System.out.println(Employee.id + " " + employee[i]);
         }
-        id=0;
     }
 
     public static void printStars() {
@@ -43,57 +44,54 @@ public class Main {
 
     // список Ф. И. О. всех сотрудников.
     public static void printFio() {
-        for (; id < employee.length - 1; id++) {
+        for (int i = 0; i < employee.length - 1; i++) {
 
-                Employee employee1 = employee[id];
-                System.out.println(employee1.getFioName());
+            Employee employee1 = employee[i];
+            System.out.println(employee1.getFioName());
         }
-        id=0;
-            }
+    }
 
     // поиск сотрудника с минимальной зарплатой.
     public static void printMinSallary() {
-        int min = 1000000;
-        for (; id < employee.length - 1; id++) {
-            Employee employee1 = employee[id];
-            int min1 = employee1.getSallary();
-            if (min1 < min) {
-                min = min1;
+        int min1 = employee[0].getSallary();
+        ;
+        for (int i = 0; i < employee.length - 1; i++) {
+            Employee employee1 = employee[i];
+            if (min1 > employee1.getSallary()) {
+                min1 = employee1.getSallary();
             }
         }
+        System.out.println(min1);
 
-        System.out.println(min);
-        id=0;
     }
-    // поиск сотрудника с максимальной зарплатой.
+        // поиск сотрудника с максимальной зарплатой.
 
     public static void printMaxSallary() {
         int max = 0;
-        for (; id < employee.length ; id++) {
-            Employee employee1 = employee[id];
-
-            int max1 = employee1.getSallary();
+        for (int i=0; i < employee.length ; i++) {
+            Employee employee1 = employee[i];
+          int max1 = employee1.getSallary();
             if (max1 > max) {
                 max = max1;
             }
         }
         System.out.println(max);
-        id=0;
     }
 
-    //    подсчет среднего значения зарплат.
+   //    подсчет среднего значения зарплат.
     public static void printAverageSallary() {
         int average = 0;
         int averageSum=0;
-        for (; id < employee.length; id++) {
-            Employee employee1 = employee[id];
-            int average1 = employee1.getSallary();
-            int sum=average+average1;
+        int sum=0;
+        for (int i=0; i < employee.length; i++) {
+            Employee employee1 = employee[i];
+           int average1 = employee1.getSallary();
+            sum=average+average1;
             average=sum;
-            averageSum=sum/employee.length;
         }
+        averageSum=sum/employee.length;
         System.out.println(averageSum);
-        id=0;
+
     }
 
-}
+    }
