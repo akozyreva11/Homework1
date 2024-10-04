a52e872126fd955ec3981818c3bbf75686d9e066
Alena Kozyreva <alena.kozyreva@paygine.net>
2024-10-04 06:01:06 +0300

    dz correct too

--- src/Main.java
+++ src/Main.java
@@ -1,99 +1,36 @@
-public class Main {
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
 
-    public static Employee[] employee = new Employee[10];
+import java.util.Arrays;
+
+public class Main {
+    public Main() {
+    }
 
     public static void main(String[] args) {
-        employee[0] = (new Employee("Иванов Петр Петрович", 1, 2));
-        employee[1] = (new Employee("Cидоров Вольф Ветрович", 2, 800));
-        employee[2] = (new Employee("Иванова Тестовая Женщина", 3, 100));
-        employee[3] = (new Employee("Карлондий Карл Витальевич", 4, 300));
-        employee[4] = (new Employee("Тестовый Иван Петрович", 5, 600));
-        employee[5] = (new Employee("Ивашкина Арнольда Дроздовна", 1, 200));
-        employee[6] = (new Employee("Осипова Петруля Петровна", 2, 900));
-        employee[7] = (new Employee("Козырева Козырела Козыревна", 3, 8900));
-        employee[8] = (new Employee("Осипова Ирина Семёновна", 4, 7800));
-        employee[9] = (new Employee("Кохинкина Петруня Сергеевна", 5, 56000));
-        employee[0].setDepartment(4);
-        employee[0].setSallary(9000);
-
-        printAll();
-        printStars();
-        printFio();
-        printStars();
-        printMinSallary();
-        printStars();
-        printMaxSallary();
-        printStars();
-        printAverageSallary();
-        er();
-
+        int[] arr = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+        int[] even = getEvenDigits(arr);
+        System.out.println();
+        System.out.println(even);
     }
 
+    public static int[] getEvenDigits(int[] values) {
+        int s = 0;
+        int j = 0;
+        int[] even = new int[values.length];
 
-    // список всех сотрудников со всеми данными,
-    public static void printAll() {
-        for (int i = 0; i < employee.length; i++) {
-
-            System.out.println( employee[i]);
-        }
-    }
-
-    public static void printStars() {
-        System.out.println("*****************************");
-        System.out.println("*****************************");
-    }
-
-    // список Ф. И. О. всех сотрудников.
-    public static void printFio() {
-        for (int i = 0; i < employee.length - 1; i++) {
-
-            Employee employee1 = employee[i];
-            System.out.println(employee1.getFioName());
-        }
-    }
-
-    // поиск сотрудника с минимальной зарплатой.
-    public static void printMinSallary() {
-        int min1 = employee[0].getSallary();
-        ;
-        for (int i = 0; i < employee.length - 1; i++) {
-            Employee employee1 = employee[i];
-            if (min1 > employee1.getSallary()) {
-                min1 = employee1.getSallary();
+        for(int i = 0; i < values.length; ++i) {
+            if (values[i] % 2 == 0) {
+                even[j] = values[i++];
+                ++j;
+                even[s] = j++;
+                ++s;
             }
         }
-        System.out.println(min1);
 
+        Arrays.sort(even, 4, 8);
+        return Arrays.copyOf(even, s);
     }
-
-        // поиск сотрудника с максимальной зарплатой.
-
-    public static void printMaxSallary() {
-        int max = 0;
-        for (int i=0; i < employee.length ; i++) {
-            Employee employee1 = employee[i];
-          int max1 = employee1.getSallary();
-            if (max1 > max) {
-                max = max1;
-            }
-        }
-        System.out.println(max);
-    }
-
-   //    подсчет среднего значения зарплат.
-    public static void printAverageSallary() {
-        int average = 0;
-        int averageSum=0;
-        int sum=0;
-        for (int i=0; i < employee.length; i++) {
-            Employee employee1 = employee[i];
-           int average1 = employee1.getSallary();
-            sum=average+average1;
-            average=sum;
-        }
-        averageSum=sum/employee.length;
-        System.out.println(averageSum);
-
-    }
-
-    }
+}
